40e4aa46bd308bf64ba8315b839000792d61d836
sherrycheng <sherrycheng@tencent.com>
2021-11-24 20:06:00 -0800

    update

--- odbo/__init__.py
+++ odbo/__init__.py
@@ -1,2 +1,2 @@
-from . import gp, prescreening, regressions, initialization, utils, featurization, bo, turbo, clustering, plot
+from . import gp, prescreening, regressions, initialization, utils, featurization, bo, turbo, plot
 from .run_exp import bo_design, turbo_design
